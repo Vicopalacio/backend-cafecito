03cfe84f33f6a25dddd57c5725e9df2d5891a2eb
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-10-24 17:07:48 -0300

    agregamos los middlewares y static

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import path from 'path';
 
 //crear una instancia de express
 const app = express();
@@ -7,6 +10,26 @@ app.set('port', process.env.PORT || 4000);
 
 app.listen(app.get('port'), ()=>{
     console.log('estoy en el puerto '+ app.get('port'))
-})
+});
 
-console.log('hola mundo');
+//midlewares: funciones que se ejecutan antes de las rutas
+app.use(cors()); //permite conexiones remotas
+
+//permiten interpretar el formato json
+app.use(express.json());
+app.use(express.urlencoded({extends:true}));
+app.use(morgan('dev'));
+//cargar un archivo estatico
+app.use(express.static(path.join(__dirname, '../public')))
+//console.log(path.join(__dirname, '../public'));
+
+//rutas
+//http://localhost:4000/prueba
+//req viene de request, res viene de response
+app.get('/prueba',(req, res)=>{
+    res.send('esto es una prueba de una peticion get')
+});
+
+app.delete('/pruebados',(req, res)=>{
+    res.send('esto es una prueba de una peticion delete')
+});
