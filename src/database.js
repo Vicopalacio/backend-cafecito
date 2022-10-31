4b732d9b2b48a3501cbd7bfc33fbc888547f2ca5
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-10-31 18:22:22 -0300

    agregamos la conexion a la base de datos de produccion

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-const url = 'mongodb://localhost:27017/cafecito';
+//const url = 'mongodb://localhost:27017/cafecito'; BD local
+const url = 'mongodb+srv://vicopalacio:vico1234@cluster0.7xqelfo.mongodb.net/cafecito';
 //const url = 'mongodb://127.0.0.1:27017/cafecito';
 
 mongoose.connect(url);
